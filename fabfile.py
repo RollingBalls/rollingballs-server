e04fcb370813d40c5ea0a4b105e8b05383487222
Giovanni Bajo <rasky@develer.com>
2015-04-11 00:29:51 +0200

    Deploy frontend too

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,6 @@
 from fabric.api import local, cd, put, env, shell_env, run
+from fabric.context_managers import lcd
+from fabric.contrib.project import rsync_project
 
 env.use_ssh_config = True
 env.hosts = ['root@server.artquest.ninja']
@@ -8,7 +10,11 @@ def deploy(what='all'):
     with shell_env(GOOS="linux", GOARCH="amd64"):
         local('godep go build')
 
+    with lcd('frontend'):
+        local('gulp build')
+
     with cd('/srv/artquest'):
-        run("stop artquest")
+        run("stop artquest || true")
+        rsync_project("/srv/artquest/static", "frontend/dist/")
         put('rollingballs-server', 'artquest-server', mode=0755)
         run("start artquest")
